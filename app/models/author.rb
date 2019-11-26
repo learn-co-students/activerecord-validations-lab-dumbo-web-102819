aad47b5185f5c9032338d2ca695c52d6238b50f9
ozubergz <jmin499@gmail.com>
2019-11-26 11:03:24 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+    # presence: validates that name is required
+    # uniqueness validates no two authors have the same name
+
+    validates :name, presence: true, uniqueness: true 
+    validates :phone_number, length: { is: 10 }
 end
