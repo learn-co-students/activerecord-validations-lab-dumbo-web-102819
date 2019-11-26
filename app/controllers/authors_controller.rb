d2526942c22aaed3235fcb408bcc1cdbc002f2b2
fanzhong <fanzhongzeng78@gmail.com>
2019-11-26 17:52:22 -0500

    Done.

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,14 @@
+class AuthorsController < ApplicationController
+	def index
+		@authors = Author.all
+	end
+
+	def show
+		@author = Author.find(params[:id])
+	end
+
+	def new
+		@author = Author.new
+	end
+
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+	validate :check_click_bait?
+
+	def check_click_bait?
+		puts "Self: #{self} and also title: #{self.title}"
+		clickbaits = [ "Won't Believe", "Secret", "Top", "Guess", "True Facts" ]
+
+        if clickbaits.include?(self.title)
+        	errors.add(:title, "#{self.title} is a clickbait.")
+        end
+	end
 end
